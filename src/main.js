e7e89cc54bff3f8b9fd661473794c8a01143b0a2
Melanie Daoheuang <daoh0031@gmail.com>
2021-03-06 11:06:44 -0700

    Change html to be appended in function

--- src/main.js
+++ src/main.js
@@ -10,18 +10,59 @@ var bottomLeft = document.querySelector('#g');
 var bottomMiddle = document.querySelector('#h');
 var bottomRight = document.querySelector('#i');
 
-window.addEventListener('load', loadGame);
-topLeft.addEventListener('click', function(event) {
-  addToken();
-  event.preventDefault();
-});
 
-function loadGame() {
 
+// window.addEventListener('load', loadGame);
+topLeft.addEventListener('click', playTopLeft);
+topMiddle.addEventListener('click', playTopMiddle);
+topRight.addEventListener('click', playTopRight);
+middleLeft.addEventListener('click', playMiddleLeft);
+middleMiddle.addEventListener('click', playMiddleMiddle);
+middleRight.addEventListener('click', playMiddleRight);
+bottomLeft.addEventListener('click', playBottomLeft);
+bottomMiddle.addEventListener('click', playBottomMiddle);
+bottomRight.addEventListener('click', playBottomRight);
+
+function   addToken(token, htmlContainer) {
+  htmlContainer.innerHTML += `
+    <p class="play-token">${token}</p>
+  `
 };
 
-function addToken() {
-   console.log('here')
+function playTopLeft() {
+  addToken('🐙', topLeft);
+};
+
+function playTopMiddle() {
+  addToken('🐙', topMiddle);
+};
+
+function playTopRight() {
+  addToken('🐙', topRight);
+};
+
+function playMiddleLeft() {
+  addToken('🐙', middleLeft);
+};
+
+function playMiddleMiddle() {
+  addToken('🐙', middleMiddle);
+};
+
+function playMiddleRight() {
+  addToken('🐙', middleRight);
+};
+
+function playBottomLeft() {
+  addToken('🐙', bottomLeft);
+};
+
+function playBottomMiddle() {
+  addToken('🐙', bottomMiddle);
+};
+
+function playBottomRight() {
+  addToken('🐙', bottomRight);
 };
 
 function show(element) {
@@ -36,3 +77,9 @@ function hide(element) {
 // Check if the game is over 
 // Next turn 
 // Available squares >> return !this.state.marks[param];
+
+// Tracks clicked cell
+// was click valid? 
+// Update game's state
+// Validate game state > has player won/draw?
+// Either stop game or change active player 
